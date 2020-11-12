37b3547517041eb97b2d99e48b0dafd70ad920a6
Othman Taik <taikothman1997@gmail.com>
2020-11-12 15:18:10 +0100

    js 6-5 updated start

---
+++ JSnack-2 Updated/js/main.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+  //Creo arrey di bici con diverse proprietà
+  const bici = [
+    {
+      nome:'mountainbike',
+      peso: 75,
+    },
+    {
+      nome:'bmx',
+      peso: 50,
+    },
+    {
+      nome:'pieghevole',
+      peso: 35,
+    },
+    {
+      nome:'corsa',
+      peso: 45,
+    },
+  ];
+    console.log(bici);
+
+  //Stampo la bici con il peso minore utilizzando destructuring e template literal
+  
+
+
+});
